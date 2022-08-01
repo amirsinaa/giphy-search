d213ae617b16330a66261234491538790c096836
Amirsina Shadkami <amirsina23@gmail.com>
2022-08-01 15:16:31 +0430

    - init: add API layer implementation using Axios

---
+++ src/api/api.ts
@@ -0,0 +1,24 @@
+import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
+
+const axiosParams = {
+  baseURL: process.env.REACT_APP_GIPHY_API_BASE_URL
+}
+
+const axiosInstance = axios.create(axiosParams);
+
+const api = (axios: AxiosInstance) => {
+  return {
+    get: <T>(url: string, config: AxiosRequestConfig = {}) =>
+      (axios.get<T>(url, config)),
+    delete: <T>(url: string, config: AxiosRequestConfig = {}) =>
+      (axios.delete(url, config)),
+    put: <T>(url: string, config: AxiosRequestConfig = {}) =>
+      (axios.put(url, config)),
+    post: <T>(url: string, config: AxiosRequestConfig = {}) =>
+      (axios.post(url, config)),
+    patch: <T>(url: string, config: AxiosRequestConfig = {}) =>
+      (axios.patch(url, config))
+  }
+}
+
+export default api(axiosInstance);
