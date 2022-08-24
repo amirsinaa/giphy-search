f3094a4549ac11613a2a75c91303fb01884c3439
Amirsina Shadkami <amirsina23@gmail.com>
2022-08-24 11:40:13 +0430

    -feature(UI implementation): add css reset styles

--- src/layouts/main/global.tsx
+++ src/layouts/main/global.tsx
@@ -3,6 +3,46 @@ import { Global } from "@emotion/react"
 export const GlobalStyles = () => (
   <Global
     styles={`
+      //CSS Reset
+      //Credit: https://www.joshwcomeau.com
+
+      *, *::before, *::after {
+        box-sizing: border-box;
+      }
+      * {
+        margin: 0;
+      }
+      html, body {
+        height: 100%;
+      }
+      body {
+        line-height: 1.5;
+        -webkit-font-smoothing: antialiased;
+      }
+      img, picture, video, canvas, svg {
+        display: block;
+        max-width: 100%;
+      }
+      input, button, textarea, select {
+        font: inherit;
+      }
+      p, h1, h2, h3, h4, h5, h6 {
+        overflow-wrap: break-word;
+      }
+      #root {
+        isolation: isolate;
+      }
+
+      //General global styles
+      body {
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+          'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+          sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+      }
+
+      //Animations
       @keyframes SpinnerRotation {
           from {
             transform: rotate(0deg);
