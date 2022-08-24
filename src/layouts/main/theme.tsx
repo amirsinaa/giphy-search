58cf4b75001be580f8702810e8936fc092ffd077
Amirsina Shadkami <amirsina23@gmail.com>
2022-08-24 11:45:31 +0430

    -feature(UI implementation): implement theme colors based on Radix-UI colors

--- src/layouts/main/theme.tsx
+++ src/layouts/main/theme.tsx
@@ -1,1 +1,20 @@
-export const theme = {}
+import {
+  violetDarkA,
+  violet,
+  mauveA
+} from '@radix-ui/colors';
+
+export const theme = {
+  colors: {
+    ...mauveA,
+    ...violet,
+    ...violetDarkA,
+
+    primary: violetDarkA.violetA6,
+    secondary: violet.violet8,
+    lightGray: mauveA.mauveA3,
+    accent: violet.violet12,
+    black: mauveA.mauveA12,
+    gray: mauveA.mauveA8,
+  }
+}
