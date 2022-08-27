af1dd5291b57a3b2f8bb5b8bc67bfb6f6c36c02e
Amirsina Shadkami <amirsina23@gmail.com>
2022-08-27 14:49:06 +0430

    -feature(UI implementation): add Giphy query string filters constants

---
+++ src/constants/giphy.ts
@@ -0,0 +1,60 @@
+export const GIPHY_QUERY = {
+  limit: [
+    {
+      id: 1000,
+      length: 1,
+    },
+    {
+      id: 1001,
+      length: 2,
+    },
+    {
+      id: 1002,
+      length: 3
+    },
+    {
+      id: 1003,
+      length: 4
+    },
+    {
+      id: 1004,
+      length: 5,
+    }
+  ],
+  rating: [
+    {
+      id: 2000,
+      age: 'g',
+    },
+    {
+      id: 2001,
+      age: 'pg',
+    },
+    {
+      id: 2002,
+      age: 'pg-13'
+    },
+    {
+      id: 2003,
+      age: 'r'
+    }
+  ],
+  languages: [
+    {
+      id: 3000,
+      language: 'en',
+    },
+    {
+      id: 3001,
+      language: 'fa',
+    },
+    {
+      id: 3002,
+      language: 'fr'
+    },
+    {
+      id: 3003,
+      language: 'du'
+    }
+  ]
+}
