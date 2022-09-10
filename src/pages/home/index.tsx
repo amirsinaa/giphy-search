c3cd8d525abb20819faf2e34d239e77ce1ed5680
Amirsina <amirsina23@gmail.com>
2022-09-10 17:05:43 +0430

    -transfer page transition wrapper to the main layout

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,13 +1,7 @@
-import PageTransition from '../../components/animation/page-transition'
 import { SearchFrom } from '../../components/search/search-from'
 
 function Home() {
-  return (
-    <PageTransition>
-      <h1>Giphy - home</h1>
-      <SearchFrom />
-    </PageTransition>
-  );
+  return <SearchFrom />;
 }
 
 export default Home;
