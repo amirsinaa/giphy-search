9ef85a9a9b1de6631310a403334ce209a18f42f9
Amirsina <amirsina23@gmail.com>
2022-09-11 22:05:53 +0430

    -add framer-motion constant transition object for gifs loading

--- src/constants/animations.ts
+++ src/constants/animations.ts
@@ -5,3 +5,29 @@ export const PAGE_ROUTING_TRANSITIONS = {
     opacity: 1,
   }
 };
+
+export const GIF_RESULTS_WRAPPER_TRANSITION = {
+  show: {
+    transition: { staggerChildren: 0.07, delayChildren: 0.2 }
+  },
+  hide: {
+    transition: { staggerChildren: 0.05, staggerDirection: -1 }
+  }
+};
+
+export const GIF_RESULTS_TRANSITION = {
+  show: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      y: { stiffness: 1000, velocity: -100 }
+    }
+  },
+  hide: {
+    y: 50,
+    opacity: 0,
+    transition: {
+      y: { stiffness: 1000 }
+    }
+  }
+}
