2cfc13c874591e7c721d21874e9b02b35d521f66
Amirsina <amirsina23@gmail.com>
2022-09-11 22:06:20 +0430

    -add react-router-dom support

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,27 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import MainLayout from './layouts/main';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
 import { AnimatePresence } from 'framer-motion'
 import Home from './pages/home';
+import About from './pages/about'
 import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 function App() {
   const [queryClient] = useState(() => new QueryClient());
   return (
     <QueryClientProvider client={queryClient}>
       <AnimatePresence exitBeforeEnter>
-        <MainLayout>
-          <Home />
-        </MainLayout>
+        <Router>
+          <MainLayout>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+            </Routes>
+          </MainLayout>
+        </Router>
       </AnimatePresence>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
