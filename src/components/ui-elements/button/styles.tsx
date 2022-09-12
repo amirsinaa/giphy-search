2b1989e4978fefdc0a9845c6b214bcb6da25683a
Amirsina <amirsina23@gmail.com>
2022-09-12 11:31:56 +0430

    -add a reusable button component

---
+++ src/components/ui-elements/button/styles.tsx
@@ -0,0 +1,31 @@
+import { theme } from '../../../layouts/main/theme';
+import styled from '@emotion/styled';
+import { Input } from '../input';
+
+export const SubmitInput = styled(Input)({
+  all: 'unset',
+});
+
+export const Button = styled.button(`
+  all: unset;
+  color: ${theme.colors.accent};
+  background: ${theme.colors.secondary};
+  padding: 4.8px 16px;
+  border-radius: 10px;
+  font-size: 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+  align-content: center;
+  width: 80%;
+  // box-shadow: 1px 2px 2px hsl(0, 0%, 68.6%),2px 4px 4px hsl(0, 0%, 68.6%);
+  transition: transform ease-in 150ms, box-shadow ease-in 150ms, background ease-in 200ms, color ease-in 200ms;
+  &:hover {
+    cursor: pointer;
+    box-shadow: 1px 2px 2px hsl(0, 0%, 68.6%),4px 8px 8px hsl(0, 0%, 68.6%);
+    color: ${theme.colors.white};
+    background: ${theme.colors.accent};
+    transform: scale(1.02);
+  }
+`);
