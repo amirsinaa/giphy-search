4904269226e1e76094e4994343790a9cfd9251d7
Amirsina <amirsina23@gmail.com>
2022-09-12 11:33:59 +0430

    -add preloader to the main layout

--- src/layouts/main/index.tsx
+++ src/layouts/main/index.tsx
@@ -1,19 +1,30 @@
+import PageTransition from '../../components/animation/page-transition'
+import { Loading } from '../../components/animation/loading';
 import Header from '../../components/skeleton/header'
 import Footer from '../../components/skeleton/footer'
 import { ThemeProvider } from '@emotion/react'
-import { theme } from './theme';
+import { useState, useEffect } from 'react';
 import { GlobalStyles } from './global';
+import { theme } from './theme';
+import { Main } from './styles';
 
 
 function MainLayout({ children }: any) {
+  const [preLoader, setPreloader] = useState(true);
+  useEffect(() => {
+    setTimeout(() => setPreloader(false), 2300);
+  }, [])
+
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Header />
-      <main>
-        {children}
-      </main>
-      <Footer />
+      {preLoader ? <Loading /> : <PageTransition>
+        <Header />
+        <Main>
+          {children}
+        </Main>
+        <Footer />
+      </PageTransition>}
     </ThemeProvider>
   )
 }
---
+++ src/layouts/main/styles.tsx
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+export const Main = styled.main`
+  flex-direction: column;
+  align-items: center;
+  min-height: 100vh;
+  display: flex;
+  width: 100%;
+`;
