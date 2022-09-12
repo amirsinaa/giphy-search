2506a2181e3ebe08e825770e5f21863f7b562a04
Amirsina <amirsina23@gmail.com>
2022-09-12 11:32:30 +0430

    -add a reusable input component

---
+++ src/components/ui-elements/input/inputs.tsx
@@ -0,0 +1,16 @@
+import { theme } from '../../../layouts/main/theme';
+import styled from '@emotion/styled';
+
+export const Input = ({ ...props }) => <input {...props} />;
+
+export const TextInput = styled.input(`
+  border-radius: 10px;
+  height: 70px;
+  width: 100%;
+  font-size: 1rem;
+  border: ${`1.5px solid ${theme.colors.secondary}`};
+  padding: 4.8px 16px;
+  margin: 0 10px;
+  &:hover {
+  }
+`);
